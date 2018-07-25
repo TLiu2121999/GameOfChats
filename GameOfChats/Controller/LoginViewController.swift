9caa9753f3f47f701f494a755bb51345ffcb0fdb
Tongtong Liu <tongtong.liu@salesforce.com>
2018-07-24 17:57:08 -0700

    First Commit

---
+++ GameOfChats/Controller/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.swift
+//  GameOfChats
+//
+//  Created by Tongtong Liu on 7/24/18.
+//  Copyright © 2018 Tongtong Liu. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = UIColor(red: 61/255, green: 91/255, blue: 151/255, alpha: 1)
+    }
+    
+
+}
